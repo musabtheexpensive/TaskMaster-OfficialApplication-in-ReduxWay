1f5509b32bac14d3223b3f4d5c6a09e66f04100a
Musab Al Mahi <musabalmahi53@gmail.com>
2024-02-19 20:04:50 +0600

    working of react hook form

--- src/pages/AddTaskModal.jsx
+++ src/pages/AddTaskModal.jsx
@@ -1,9 +1,18 @@
+import { useForm } from "react-hook-form";
 import Modal from "../components/ui/Modal";
 
-const AddTaskModal = ({isOpen,setIsOpen}) => {
+const AddTaskModal = ({ isOpen, setIsOpen }) => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Whatever">
-      <p>lorem</p>
+      <form onSubmit={() => handleSubmit(onSubmit)}>
+        <input type="text" {...register("name")} />
+        <button type="submit">Submit</button>       
+      </form>
     </Modal>
   );
 };
