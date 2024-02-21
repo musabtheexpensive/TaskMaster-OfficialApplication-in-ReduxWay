da8be20ce1fda44baf55ae314aee1206e7c134a7
Musab Al Mahi <musabalmahi53@gmail.com>
2024-02-21 16:18:14 +0600

    created a new folder which name is rtkQuery Folder. and exploring rtk query data fetching in redux way

--- TaskMaster-Redux-@Way-simpleApplication/src/components/tasks/MyTasks.jsx
+++ TaskMaster-Redux-@Way-simpleApplication/src/components/tasks/MyTasks.jsx
@@ -2,15 +2,23 @@ import {
   CheckIcon,
   DocumentMagnifyingGlassIcon,
 } from "@heroicons/react/24/outline";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateStatus, userTasks } from "../../redux/features/tasks/tasksSlice";
+import TasksDetailsModal from "../../pages/TasksDetailsModal";
 
 const MyTasks = () => {
   const { tasks, userSpecificTasks } = useSelector((state) => state.tasksSlice);
   const { name: userName } = useSelector((state) => state.userSlice);
+  const [isOpen, setIsOpen] = useState(false);
+  const [taskId, setTaskId] = useState(0);
 
   console.log(name);
+
+  const handleModal = (id) => {
+    setTaskId(id);
+    setIsOpen(!isOpen);
+  };
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -19,6 +27,7 @@ const MyTasks = () => {
 
   return (
     <div>
+      <TasksDetailsModal isOpen={isOpen} setIsOpen={setIsOpen} id={taskId} />
       <h1 className="text-xl my-3">My Tasks</h1>
       <div className=" h-[750px] overflow-auto space-y-3">
         {userSpecificTasks?.map((item) => (
@@ -28,7 +37,11 @@ const MyTasks = () => {
           >
             <h1>{item.title}</h1>
             <div className="flex gap-3">
-              <button className="grid place-content-center" title="Details">
+              <button
+                onClick={() => handleModal(item.id)}
+                className="grid place-content-center"
+                title="Details"
+              >
                 <DocumentMagnifyingGlassIcon className="w-5 h-5 text-primary" />
               </button>
               <button
