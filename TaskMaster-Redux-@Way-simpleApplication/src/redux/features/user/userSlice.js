fa8e8a876fac0963250e689c97eec2f7a0c4d386
Musab Al Mahi <musabalmahi53@gmail.com>
2024-02-28 13:18:53 +0600

    handling user persistency in signup into main web page

--- TaskMaster-Redux-@Way-simpleApplication/src/redux/features/user/userSlice.js
+++ TaskMaster-Redux-@Way-simpleApplication/src/redux/features/user/userSlice.js
@@ -28,7 +28,16 @@ export const createUser = createAsyncThunk(
 const userSlice = createSlice({
   name: "userSlice",
   initialState,
-  reducers: {},
+  reducers: {
+    setUser: (state, { payload }) => {
+      state.name = payload.name;
+      state.email = payload.email;
+    },
+    toggleLoading: (state, { payload }) => {
+      state.isLoading = payload;
+    },
+  },
+
   extraReducers: (builder) => {
     builder
       .addCase(createUser.pending, (state) => {
@@ -54,4 +63,5 @@ const userSlice = createSlice({
       });
   },
 });
+export const { setUser, toggleLoading } = userSlice.actions;
 export default userSlice.reducer;
